5d8f3fd33f9a86b98a86141985d48f51432717c5
Oqiljon Islomov <o.islomov@tuit.uz>
2024-05-10 13:54:29 +0500

    Update urls.py

--- edusystem/urls.py
+++ edusystem/urls.py
@@ -44,4 +44,5 @@ urlpatterns = [
     path('korinmas_api/',include('dj_rest_auth.urls')),
     path('',schema_view.with_ui('swagger',cache_timeout=0),name='schema-swagger-ui')
     #path('swagger/',schema_view.with_ui('swagger',cache_timeout=0),name='schema-swagger-ui')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
