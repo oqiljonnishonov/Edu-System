5ce9e07677a2788d10e1223b5fecad9fd13f17f7
Oqiljon Islomov <o.islomov@tuit.uz>
2024-05-11 15:23:55 +0500

    Update settings.py

--- edusystem/settings.py
+++ edusystem/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'systemapp.middleware.APIUsageMiddleware',
 ]
 
 ROOT_URLCONF = "edusystem.urls"
